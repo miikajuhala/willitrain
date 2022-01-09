ca8bf23d9194f7aadc12105b9113b9816f16bb89
Miika Juhala <77334107+miikajuhala@users.noreply.github.com>
2022-01-09 12:46:26 +0200

    first commit

---
+++ src/main/java/com/example/willitrain/WillitrainApplication.java
@@ -0,0 +1,13 @@
+package com.example.willitrain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WillitrainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WillitrainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/willitrain/WillitrainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.willitrain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WillitrainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
