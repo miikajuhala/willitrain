b24ee51e52508e54138849c2d879fd2001a0c8fb
Miika Juhala <77334107+miikajuhala@users.noreply.github.com>
2022-01-09 23:18:16 +0200

    basic JWT security

--- src/main/java/com/example/willitrain/WebSecurityConfig.java
+++ src/main/java/com/example/willitrain/WebSecurityConfig.java
@@ -1,24 +1,75 @@
 package com.example.willitrain;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import com.example.willitrain.web.AuthenticationFilter;
+import com.example.willitrain.web.LoginFilter;
+
+import  com.example.willitrain.web.UserDetailServiceImpl;
+
+
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-@Override
-protected void configure(HttpSecurity http) throws Exception {
- http
- .authorizeRequests()
- .antMatchers("/api/*", "/*").permitAll()
- .anyRequest().authenticated()
- .and()
- .formLogin()
- .permitAll()
-.and() 
- .logout()
- .permitAll();
- }
+
+	@Autowired
+	private UserDetailServiceImpl userDetailsService; 
+	
+	@Override
+    protected void configure(HttpSecurity http) throws Exception {
+     http.csrf().disable().cors().and().authorizeRequests()
+      .antMatchers(HttpMethod.POST, "/login","/loginpage","/api/register").permitAll()
+      .antMatchers(HttpMethod.GET, "/api", "/api/**","/role","/loginpage","/saveuser","/signup").permitAll()
+      .antMatchers(HttpMethod.PUT, "/api", "/api/**").permitAll()
+     // .antMatchers(HttpMethod.GET, "api/spots", "api/users**").hasRole("admin")
+      .antMatchers(HttpMethod.POST, "/api/spots/add","/saveuser","/api/admin/newuser").authenticated()
+      .antMatchers(HttpMethod.DELETE, "/api/spots/delete/*").permitAll()
+          .anyRequest().authenticated()
+          .and()
+          // Filter for the api/login requests
+          .addFilterBefore(new LoginFilter("/login",
+           authenticationManager()),
+                  UsernamePasswordAuthenticationFilter.class)
+          // Filter for other requests to check JWT in header
+          .addFilterBefore(new AuthenticationFilter(),
+                  UsernamePasswordAuthenticationFilter.class);
+    }
+	
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+	  auth.userDetailsService(userDetailsService)
+	  .passwordEncoder(new BCryptPasswordEncoder());
+	}
+	
+	@Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        UrlBasedCorsConfigurationSource source = 
+            new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(Arrays.asList("http://localhost:3000/"));
+        config.setAllowedMethods(Arrays.asList("*"));
+        config.setAllowedHeaders(Arrays.asList("*")); 
+        config.setAllowCredentials(true);
+        config.applyPermitDefaultValues();
+        
+        source.registerCorsConfiguration("/**", config);
+        return source;
+  }
+	
 }
---
+++ src/main/java/com/example/willitrain/web/UserDetailServiceImpl.java
@@ -0,0 +1,28 @@
+package com.example.willitrain.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+
+
+@Service
+public class UserDetailServiceImpl implements UserDetailsService {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User currentUser = userRepository.findByUsername(username);
+        UserDetails user = new org.springframework.security.core
+            .userdetails.User(username, currentUser.getPassword()
+            , true, true, true, true, 
+            AuthorityUtils.createAuthorityList(currentUser.getRole()));
+        return user;
+	}
+
+}
